scribbr/assessment-full-stack

Initialize the assessments boilerplate

workspaces/client/src/App.tsx
@@ -1,0 +1,13 @@
+import { APP_TITLE } from "@scribbr-assessment-full-stack/common";
+import React, { useState } from "react";
+
+import "./index.css";
+
+export function App() {
+  return (
+    <div>
+      <h1>Welcome to {APP_TITLE}!</h1>
+      <p>This would probably be the starting point of your app.</p>
+    </div>
+  );
+}

DarraghFinn/Favourite-Movies

workspaces/client/src/App.tsx
@@ -1,0 +1,13 @@
+import { APP_TITLE } from "@scribbr-assessment-full-stack/common";
+import React, { useState } from "react";
+
+import "./index.css";
+
+export function App() {
+  return (
+    <div>
+      <h1>Welcome to {APP_TITLE}!</h1>
+      <p>This would be probably the starting point of your app.</p>
+    </div>
+  );
+}